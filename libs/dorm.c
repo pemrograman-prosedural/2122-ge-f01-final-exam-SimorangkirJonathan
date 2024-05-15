663ec5a8123aa4b771981ad39376861717cd759f
SimorangkirJonathan <143678943+SimorangkirJonathan@users.noreply.github.com>
2024-05-15 15:54:28 +0700

    update

--- libs/dorm.c
+++ libs/dorm.c
@@ -1,27 +1,42 @@
 #include "dorm.h"
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
-struct dorm_t create_dorm(char *input)
+struct dorm_t create_dorm(char *_name, unsigned short _capacity, enum gender_t _gender)
 {
-  struct dorm_t drm;
-  strcpy(drm.name, strtok(NULL, "#"));
-  drm.capacity = atoi(strtok(NULL, "#"));
-  strcpy(drm.gender, strtok(NULL, "#"));
-  drm.residents_num=0;
-  return drm;
+    struct dorm_t d;
+    strcpy(d.name, _name);
+    d.capacity = _capacity;
+    d.gender = _gender;
+    d.residents_num = 0;
+    return d;
 }
 
-int find_dorm(char *asrama, int zdrm, struct dorm_t *drm)
+void print_dorm(struct dorm_t *_dorm, int count)
 {
-  int find_dorm;
-  for (int m = 0; m < zdrm; m++)
-  {
-    if (strcmp(asrama, drm[m].name)==0)
+    for (int i = 0; i < count; i++)
     {
-      find_dorm=m;
+        if (_dorm[i].gender == GENDER_MALE)
+        {
+            printf("%s|%d|male\n", _dorm[i].name, _dorm[i].capacity);
+        }
+        else if (_dorm[i].gender == GENDER_FEMALE)
+        {
+            printf("%s|%d|female\n", _dorm[i].name, _dorm[i].capacity);
+        }
     }
-  }
-  return find_dorm;
+}
+void print_dorm_detail(struct dorm_t *_dorm, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        if (_dorm[i].gender == GENDER_MALE)
+        {
+            printf("%s|%d|male|%d\n", _dorm[i].name, _dorm[i].capacity, _dorm[i].residents_num);
+        }
+        else if (_dorm[i].gender == GENDER_FEMALE)
+        {
+            printf("%s|%d|female|%d\n", _dorm[i].name, _dorm[i].capacity, _dorm[i].residents_num);
+        }
+    }
 }
